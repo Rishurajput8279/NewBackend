78931ad807c9df86c24cebfb5d18922bb809794c
Reshu Jaynt <reshu.22b1531032@abes.ac.in>
2025-09-10 23:00:56 +0530

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error :",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at Port :${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log(`MONGO DB connection fail: !!!`,err)
+})
 
 
 
